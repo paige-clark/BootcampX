3fb5656b5e9ba75dc2c131dfa824327a11d40b2e
paige-clark <paigeclarkillustration@gmail.com>
2022-08-25 00:10:14 +0000

    Add two queries made using pg and process.argv

--- 4_queries/12_teacher_assist_names.sql
+++ 4_queries/12_teacher_assist_names.sql
@@ -1,11 +1,11 @@
-SELECT DISTINCT teachers.name AS teacher,
-       cohorts.name AS cohort
+SELECT DISTINCT teachers.name as teacher,
+                cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests
-  ON teachers.id = assistance_requests.teacher_id
+  ON teacher_id = teachers.id
 JOIN students
-  ON students.id = assistance_requests.student_id
+  ON student_id = students.id
 JOIN cohorts
-  ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'JUL02'; 
-GROUP BY teacher;
+  ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher,
+                cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests
+  ON teacher_id = teachers.id
+JOIN students
+  ON student_id = students.id
+JOIN cohorts
+  ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`, [process.argv[2]])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
